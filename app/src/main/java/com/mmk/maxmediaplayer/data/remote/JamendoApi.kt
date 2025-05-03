4e5962f649edd435147e41ffc24ebff7467c5944
MahabubKarim <mahabub.karim21@gmail.com>
2025-05-03 11:03:04 +0600

    hide the Jamendo Api Key from the project.

--- app/src/main/java/com/mmk/maxmediaplayer/data/remote/JamendoApi.kt
+++ app/src/main/java/com/mmk/maxmediaplayer/data/remote/JamendoApi.kt
@@ -1,6 +1,7 @@
 package com.mmk.maxmediaplayer.data.remote
 
 import com.mmk.maxmediaplayer.data.model.remote.ApiResponse
+import com.mmk.maxmediaplayer.util.LocalProperties
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -8,7 +9,8 @@ import retrofit2.http.Query
 interface JamendoApi {
     @GET("tracks")
     suspend fun getTracks(
-        @Query("client_id") clientId: String = "e254815f",
+        @Query("client_id") clientId: String = LocalProperties.getProperty("JAMENDO_API_KEY"),
+        //@Query("client_id") clientId: String = BuildConfig.CLIENT_ID,
         @Query("format") format: String = "jsonpretty",
         @Query("limit") limit: Int = 20
     ): Response<ApiResponse>
---
+++ app/src/main/java/com/mmk/maxmediaplayer/util/LocalProperties.kt
@@ -0,0 +1,17 @@
+package com.mmk.maxmediaplayer.util
+
+import java.io.File
+import java.util.*
+
+object LocalProperties {
+    private val properties = Properties().apply {
+        val localProperties = File("local.properties")
+        if (localProperties.exists()) {
+            load(localProperties.inputStream())
+        }
+    }
+
+    fun getProperty(key: String, default: String = ""): String {
+        return properties.getProperty(key, default)
+    }
+}
