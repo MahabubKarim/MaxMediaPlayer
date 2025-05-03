cfb3e6ed1411f1d135aff7ad60310528160ae18b
MahabubKarim <mahabub.karim21@gmail.com>
2025-05-03 10:32:35 +0600

    updated .gitignore

--- app/src/main/java/com/mmk/maxmediaplayer/domain/usecase/GetTracksUseCase.kt
+++ app/src/main/java/com/mmk/maxmediaplayer/domain/usecase/GetTracksUseCase.kt
@@ -1,5 +1,7 @@
 package com.mmk.maxmediaplayer.domain.usecase
 
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.Modifier.Element
 import com.mmk.maxmediaplayer.domain.model.Track
 import com.mmk.maxmediaplayer.domain.repository.MusicRepository
 import com.mmk.maxmediaplayer.domain.utils.Resource
@@ -16,6 +18,26 @@ import javax.inject.Inject
 class GetTracksUseCase @Inject constructor(
     private val repository: MusicRepository
 ) {
+    /**
+     * In Kotlin, operator fun invoke() allows instances of a class to be called like functions.
+     * When the invoke operator is defined for a class, you can call an instance of that class using
+     * parentheses, similar to calling a regular function. This feature enhances code readability
+     * and conciseness, and it is often used in DSL (Domain Specific Language) creation, encapsulating
+     * functionality, or creating more natural syntax.
+     *
+     * ```
+     * class Greeter(val greeting: String) {
+     *     operator fun invoke(target: String) = "$greeting, $target!"
+     * }
+     *
+     * val hello = Greeter("Hello")
+     * println(hello("World")) // Output: Hello, World!]
+     * ```
+     *
+     * In this example, hello is an instance of the Greeter class. Because the invoke operator is
+     * defined, hello("World") is equivalent to hello.invoke("World"). This makes the code more
+     * expressive and easier to read.
+     */
     operator fun invoke(
         forceRefresh: Boolean = false
     ): Flow<Resource<List<Track>>> = flow {
